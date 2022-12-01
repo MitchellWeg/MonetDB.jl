2305c54d7f93fec164c7b8f85866e41a70622343
MitchellWeg <mitchell.w@live.nl>
2022-12-01 13:09:16 +0100

    working mapi protocol

--- src/Mapi.jl
+++ src/Mapi.jl
@@ -19,16 +19,27 @@ end
 # Establishes a connection, and parses the challenge.
 #
 function mapi_connect(params)
-    socket = Sockets.connect("127.0.0.1", params.port)
-    challenge = read(socket)
-    print(challenge)
+    socket = Sockets.connect(params.hostname, params.port)
 
     conn = MapiConnection(params, socket)
+    challenge = get_block(conn)
 
     parsed_challenge = parse_server_challenge(challenge, params)
-    print(parsed_challenge)
 
     put_block(conn, parsed_challenge)
+
+    request_for_authentication = get_block(conn)
+
+    print(request_for_authentication)
+    if startswith(request_for_authentication, "^mapi:merovingian:")
+        new_challenge = get_block(conn)
+        new_parsed_challenge = parse_server_challenge(new_challenge, params)
+        put_block(conn, new_parsed_challenge)
+    end
+
+    authenticated = get_block(conn)
+
+    print(authenticated)
 
     return conn
 end
@@ -42,8 +53,7 @@ function parse_server_challenge(challenge, params)
     hashed_password = bytes2hex(sha512(params.password)) * salt
     hash = bytes2hex(sha1(hashed_password))
 
-    return "SUIIIII"
-    # return "LIT:$(params.username):{SHA1}$(hash):sql:$(params.database):\n"
+    return "LIT:$(params.username):{SHA1}$(hash):sql:$(params.database):\n"
 end
 
 function put_block(conn, msg)
@@ -51,15 +61,20 @@ function put_block(conn, msg)
     byte_count = length(msg)
 
     flag = UInt16((byte_count << 1) + is_last)
-    print(flag)
-
-    payload = Vector{UInt8}(msg)
 
     write(conn.socket, flag);
     write(conn.socket, msg);
+end
 
-    resp = read(conn.socket)
-    print("resp:\n")
-    print(resp)
 
+"""
+Gets the block, and transforms it into a coherent String.
+TODO: Do something if this is not the last block.
+"""
+function get_block(conn)
+    raw_data = readavailable(conn.socket)
+
+    data  = String(raw_data[3:end])
+
+    return data
 end
